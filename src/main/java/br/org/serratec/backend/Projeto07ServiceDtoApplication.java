c8bbbccadd6a85db427c5f2d87f70f2078b2b0d1
PatriciaBraganca <patricia.rbraganca@gmail.com>
2021-11-04 18:47:50 -0300

    Nosso primeiro commit - Criação do Projeto

---
+++ src/main/java/br/org/serratec/backend/Projeto07ServiceDtoApplication.java
@@ -0,0 +1,13 @@
+package br.org.serratec.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto07ServiceDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto07ServiceDtoApplication.class, args);
+	}
+
+}
